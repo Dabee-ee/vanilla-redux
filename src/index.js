bbe0e65e84c33987c40d3fc8793e7b37fde0bd15
Dabee-ee <movie2529@naver.com>
2022-08-18 04:34:37 +0900

    Create Action

--- src/index.js
+++ src/index.js
@@ -1,41 +1,26 @@
 import { createStore } from "redux";
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
 
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
 console.log(countStore.getState());
 
-/*
-
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
-const countModifier = (count = 0) => {
-  return count;
-};
-
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-const countStore = createStore(countModifier);
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-console.log(countStore.getState());
-
- */
+// 개발자가 리듀서에게 메세지를 보내는 방법
+// countModifier(currentState = 0, { type: "Hello" })
