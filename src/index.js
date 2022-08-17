ad31eef09c5f00ba9e584ab142f921102289b7c6
Dabee-ee <movie2529@naver.com>
2022-08-18 04:54:54 +0900

    Refactor Redux-Countdown

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -25,8 +28,8 @@ const onchange = () => {
 
 countStore.subscribe(onchange);
 
-const handleAdd = () => countStore.dispatch({ type: "ADD" });
-const handleMinus = () => countStore.dispatch({ type: "MINUS" });
+const handleAdd = () => countStore.dispatch({ type: ADD });
+const handleMinus = () => countStore.dispatch({ type: MINUS });
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
