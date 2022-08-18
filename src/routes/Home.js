4d82f60a7c293b6e238b6e3f17cb859e35493b81
Dabee-ee <movie2529@naver.com>
2022-08-18 16:33:46 +0900

    MapState & MapDispatch -> UseSelector & UseDispatch

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToDo } from "../store";
 
-function Home() {
+const Home = () => {
   const [text, setText] = useState("");
-  function onChange(e) {
-    setText(e.target.value);
-  }
-  function onSubmit(e) {
-    e.preventDefault();
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+  const toDo = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addToDo(text));
     setText("");
-  }
+  };
   return (
     <>
       <h1>To do</h1>
@@ -16,9 +21,19 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button onClick={onSubmit}>등록</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDo)}</ul>
     </>
   );
-}
+};
+
+// function mapStateToProps(state, ownProps) {
+//   return { toDos: state };
+// }
+
+// function mapDispatchToProps(dispatch) {
+//   return { adToDo: (text) => dispatch(actionCreators.adToDo(text)) };
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Home);
 
 export default Home;
